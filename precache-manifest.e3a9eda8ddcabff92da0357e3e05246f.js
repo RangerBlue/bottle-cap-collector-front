9c1f87f481c38b19ce572c3dace24a364e6351b8
Kamil Machul <kamil.machul@gmail.com>
2021-01-06 00:43:19 +0100

    Updates

---
+++ precache-manifest.e3a9eda8ddcabff92da0357e3e05246f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d3eda8b2378878b8b2cd66f5a0e1997",
+    "url": "/bottle-cap-collector-front/index.html"
+  },
+  {
+    "revision": "6d453b755ad6a53a04fa",
+    "url": "/bottle-cap-collector-front/static/css/main.20c4a973.chunk.css"
+  },
+  {
+    "revision": "7f06b26fd25d12d6edf5",
+    "url": "/bottle-cap-collector-front/static/js/2.b4551fa8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bottle-cap-collector-front/static/js/2.b4551fa8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d453b755ad6a53a04fa",
+    "url": "/bottle-cap-collector-front/static/js/main.8bf6908d.chunk.js"
+  },
+  {
+    "revision": "1afe6c5591e07eb3685c",
+    "url": "/bottle-cap-collector-front/static/js/runtime-main.565a8f81.js"
+  }
+]);
